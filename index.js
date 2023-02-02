fe8707d7812e70cd9df2a260a8a31cf0e06756ec
JeanLoic GARCIA <(mailto:garcia.jeanloic@gmail.com)>
2023-02-02 17:53:31 +0100

    add get rats / vitesse / contagion / menace

--- index.js
+++ index.js
@@ -8,13 +8,46 @@ app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.get("/test", (req, res) => {
+app.get("/rats", (req, res) => {
   database
     .query(
       "SELECT * from rats inner join vitesse on rats.vitesse = vitesse.id inner join contagion on rats.contagion = contagion.id inner join menace on rats.menace = menace.id"
     )
     .then(([rats]) => {
       res.json(rats);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+});
+app.get("/vitesse", (req, res) => {
+  database
+    .query("SELECT vitesseTexte from vitesse")
+    .then(([vitesseText]) => {
+      res.json(vitesseText);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+});
+app.get("/menace", (req, res) => {
+  database
+    .query("SELECT menaceTexte from menace")
+    .then(([menaceTexte]) => {
+      res.json(menaceTexte);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+});
+app.get("/contagion", (req, res) => {
+  database
+    .query("SELECT contagionTexte from contagion")
+    .then(([contagionTexte]) => {
+      res.json(contagionTexte);
     })
     .catch((err) => {
       console.error(err);
